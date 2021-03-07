1e8e94be4174c68248b2e8c735652cc673c6e468
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2021-03-07 22:42:04 +0100

    added translations and support for multiple languages

--- kyivmural/__init__.py
+++ kyivmural/__init__.py
@@ -1,13 +1,20 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, current_app
+from flask_babel import Babel
+from config import Config
+
+babel = Babel()
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
 
+    app.config.from_object(config_class)
     app.config["KYIVMURAL_API_ENDPOINT"] = os.environ["KYIVMURAL_API_ENDPOINT"]
     app.config["AWS_REGION"] = os.environ["AWS_DEFAULT_REGION"]
+
+    babel.init_app(app)
 
     from kyivmural.main import bp as main_bp
     app.register_blueprint(main_bp)
@@ -16,3 +23,9 @@ def create_app():
     app.register_blueprint(errors_bp)
 
     return app
+
+
+@babel.localeselector
+def get_locale():
+    return "en"
+    return request.accept_languages.best_match(current_app.config['LANGUAGES'])
