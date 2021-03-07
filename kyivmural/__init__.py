053511b52528ef96568a4f522493850cceed7211
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2021-03-07 16:57:55 +0100

    added  comment and pylint disable statement

--- kyivmural/__init__.py
+++ kyivmural/__init__.py
@@ -6,4 +6,6 @@ app = Flask(__name__)
 app.config["KYIVMURAL_API_ENDPOINT"] = os.environ["KYIVMURAL_API_ENDPOINT"]
 app.config["AWS_REGION"] = os.environ["AWS_DEFAULT_REGION"]
 
-from kyivmural import routes
+# import here as described in as described in
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+from kyivmural import routes  # pylint: disable=wrong-import-position
